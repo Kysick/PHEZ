ee8b037ce875760b6b42b4ea411c99f4e7ab0a5b
FeDDoSDudos <frolov.fiodor2015@yandex.ru>
2019-04-24 19:38:38 +0300

    Merge branch 'master' of https://github.com/Kysick/PHEZ
    # Conflicts:
    #	app/src/main/assets/physicsthemesdb.db
    #	app/src/main/java/com/onisq/phez/DatabaseHelper.java

--- app/src/main/java/com/onisq/phez/DatabaseHelper.java
+++ app/src/main/java/com/onisq/phez/DatabaseHelper.java
@@ -15,7 +15,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static String DB_NAME = "physicsthemesdb.db";
     private static String DB_PATH = "";
     //DB VERSION
-    private static final int DB_VERSION = 3;
+    private static final int DB_VERSION = 4;
 
     private SQLiteDatabase mDataBase;
     private final Context mContext;
