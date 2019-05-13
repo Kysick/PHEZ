c1e22ca8a1d4e17366c3c4ce3a8982daa92262ec
Kysick <40487343+Kysick@users.noreply.github.com>
2019-05-13 17:49:42 +0300

    yeah

--- app/src/main/java/com/onisq/phez/MainActivity.java
+++ app/src/main/java/com/onisq/phez/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setElevation(0);
+       // ActionBar actionBar = getSupportActionBar();
+        //actionBar.setElevation(0);
 
     }
 
