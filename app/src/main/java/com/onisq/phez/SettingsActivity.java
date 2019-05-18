e321bcf9f7974f787ffe52a216c95976f3badfe9
Kysick <40487343+Kysick@users.noreply.github.com>
2019-05-18 13:38:00 +0300

    ZakoniSohraneniya

--- app/src/main/java/com/onisq/phez/SettingsActivity.java
+++ app/src/main/java/com/onisq/phez/SettingsActivity.java
@@ -1,15 +1,29 @@
 package com.onisq.phez;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.CardView;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class SettingsActivity extends AppCompatActivity {
     private Switch mySwitch;
     SharedPref sharedPref;
+    private CardView cardView;
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         sharedPref = new SharedPref(this);
@@ -38,6 +52,14 @@ public class SettingsActivity extends AppCompatActivity {
                 }
             }
         });
+
+        cardView = (CardView) findViewById(R.id.settingCard);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callAlert();
+            }
+        });
     }
 
     @Override
@@ -46,4 +68,72 @@ public class SettingsActivity extends AppCompatActivity {
         i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
+
+
+    private void callAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Удаление прогресса");
+        builder.setMessage("Вы уверены?");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                callSecondAlert();
+            }
+        });
+        builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    private void callSecondAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Удаление прогресса");
+        builder.setMessage("Вы ТОЧНО уверены?");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                deleteProgress();
+                Toast.makeText(SettingsActivity.this, "Прогресс удален", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    private void deleteProgress(){
+        mDBHelper = new DatabaseHelper(this);
+
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+        Cursor cursor = mDb.rawQuery("select * from stats", null);
+        cursor.moveToFirst();
+        ContentValues cv = new ContentValues();
+        cv.put("Points",0);
+        mDb.update("stats", cv,null, null);
+    }
+
+
+
+
 }
